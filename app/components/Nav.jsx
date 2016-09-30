7ed6aadfd7bba355d07bbbe3fdc8c177caccd7ea
rayGreycloud <rayGreycloud@gmail.com>
2016-09-30 14:13:28 -0700

    Refactor nav for BEM

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,23 +5,23 @@ const Nav = () => {
 
   return (
     <div className="navbar">
-      <div className="top-bar" id="responsive-menu">
+      <div className="nav__top-bar" id="responsive-menu">
         <div className="top-bar-left">
-          <ul className="menu">
-            <li className="menu-text">
+          <ul className="nav__menu">
+            <li className="nav__menu-text">
               <IndexLink to='/' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>rayGREYCLOUD</IndexLink>
             </li>
           </ul>
         </div>
         <div className='top-bar-right'>
-          <ul className="menu">
-            <li className="menu-text">
+          <ul className="nav__menu">
+            <li className="nav__menu-text">
               <Link to='/about' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>ABOUT</Link>
             </li>
-            <li className="menu-text">
+            <li className="nav__menu-text">
               <Link to='/projects' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>PROJECTS</Link>
             </li>
-            <li className="menu-text">
+            <li className="nav__menu-text">
               <Link to='/contact' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>CONTACT</Link>
             </li>
           </ul>
